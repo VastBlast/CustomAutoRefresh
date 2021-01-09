60a5ec00ff3236832b7dbf59f4f3c1572bc387c8
VastBlast <48421698+VastBlast@users.noreply.github.com>
2021-01-08 21:00:13 -0500

    timer badge addition
    allows users to see the amount of time in seconds, remaining until the timer will refresh the page.

--- main/assets/js/background.js
+++ main/assets/js/background.js
@@ -1,5 +1,24 @@
 tabIntervals = {};
 
+let getIntervalTime = (() => { //allows us to get the remaining time in an interval
+    let _setInterval = setInterval, // Reference to the original setInterval
+    intervalMap = {}; // intervalMap of all Intervals with their end times
+
+    setInterval = (callback, delay) => { // Modify setInterval
+        let id = _setInterval(function () {
+            callback();
+            intervalMap[id] = Date.now() + delay; // Store the end time
+        }, delay); // Run the original, and store the id
+        intervalMap[id] = Date.now() + delay; // Store the end time
+        return id; // Return the id
+    };
+
+    return (id) => { // The actual getInterval function
+        // If there was no Interval with that id, return NaN, otherwise, return the time left clamped to 0
+        return intervalMap[id] ? Math.max(intervalMap[id] - Date.now(), 0) : NaN;
+    }
+})();
+
 function refreshTab(tabId) {
     return new Promise((resolve, reject) => {
         try {
@@ -31,10 +50,28 @@ function getTab(tabId) {
 }
 
 function startRefresh(tab, interval) {
-    const tabId = tab.id;
+    var tabId = tab.id;
+    let intervalSec = Math.round(interval / 1000);
     stopRefresh(tabId);
     tabIntervals[tabId] = {};
+    tabIntervals[tabId]['intervalTime'] = intervalSec;
+
     refreshTab(tabId); //start it off
+    const badgeInterval = setInterval(function () {
+        if (tabIntervals[tabId] == undefined) {
+            clearInterval(badgeInterval);
+            return;
+        }
+        const intervalSecLeft = Math.round(getIntervalTime(tabIntervals[tabId]['interval']) / 1000);
+        chrome.browserAction.setBadgeText({
+            tabId: tabId,
+            text: intervalSecLeft.toString()
+        });
+        if (intervalSecLeft < 1) {
+            clearInterval(badgeInterval);
+        }
+    }, 1100);
+
     tabIntervals[tabId]['interval'] = setInterval(async function () {
         try {
             const tab = await getTab(tabId);
@@ -49,12 +86,28 @@ function startRefresh(tab, interval) {
             } else {
                 await refreshTab(tabId);
             }
-            //chrome.browserAction.setBadgeText({tabId:tabId, text:"REFRESH"});
+            if (intervalSec > 1) {
+                const badgeInterval = setInterval(function () {
+                    if (tabIntervals[tabId] == undefined) {
+                        clearInterval(badgeInterval);
+                        return;
+                    }
+                    const intervalSecLeft = Math.round(getIntervalTime(tabIntervals[tabId]['interval']) / 1000);
+                    chrome.browserAction.setBadgeText({
+                        tabId: tabId,
+                        text: intervalSecLeft.toString()
+                    });
+                    if (intervalSecLeft < 1) {
+                        clearInterval(badgeInterval);
+                    }
+                }, 1100);
+            }
         } catch (e) {
             console.log(e);
             stopRefresh(tabId);
             return;
         }
+        return;
     }, interval);
 
     chrome.tabs.onUpdated.addListener(function (changedTabId, changeInfo, tab) {
@@ -69,7 +122,6 @@ function startRefresh(tab, interval) {
         }
     });
 
-    tabIntervals[tabId]['intervalTime'] = interval / 1000;
 }
 
 function stopRefresh(tabId) {
@@ -80,6 +132,10 @@ function stopRefresh(tabId) {
         chrome.browserAction.setIcon({
             tabId: tabId,
             path: "./icons/icon-inactive.png"
+        });
+        chrome.browserAction.setBadgeText({
+            tabId: tabId,
+            text: ""
         });
     }
 }
